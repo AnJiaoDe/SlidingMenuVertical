da7bb54de442d9456e2e56f19b879ebedad3d257
437042721 <437042721@qq.com>
2018-04-25 22:37:16 +0800

    ADDED

---
+++ app/src/main/java/com/cy/slidingmenuvertical/BaseActivity.java
@@ -0,0 +1,28 @@
+package com.cy.slidingmenuvertical;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.cy.translucentparent.StatusNavUtils;
+
+/**
+ * Created by cy on 2018/4/25.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        StatusNavUtils.setStatusBarColor(this,0x00000000);
+    }
+    public void startAppcompatActivity(Class<?> cls) {
+        startActivity(new Intent(this, cls));
+    }
+
+
+}
